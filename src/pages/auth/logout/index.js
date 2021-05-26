c8bd7162c7515be6afdb7e4c87af5e40ac920104
Ayushman Tripathy <ayushmantripathy9@gmail.com>
2021-05-26 23:59:38 +0530

    Authentication Finalised

--- src/pages/auth/logout/index.js
+++ src/pages/auth/logout/index.js
@@ -1,19 +1,43 @@
 import React from 'react'
-import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
+import { Button, Form, Grid, Header, Image, Loader, Message, Segment } from 'semantic-ui-react'
 import axios from "axios"
-import { apiLogin, apiSignUp } from "../../../urls"
+import { apiLogin, apiSignUp, apiLogout } from "../../../urls"
 import { routeHome } from "../../../urls"
 import { Redirect, Router } from 'react-router'
 import Home from '../../home'
+import { getCookie } from '../../../utils'
 class Logout extends React.Component {
-  render() {
-    return(
-        <div>
-            
-        </div>
-    );
 
-  }
+    constructor(props) {
+        super(props)
+        this.state = {
+            loaded: null
+        }
+    }
+    componentDidMount() {
+        axios.post(`${apiLogout()}`, {
+            session_id: getCookie("session_id")
+        }).then(
+            res => {
+                console.log(res)
+                window.location = "/"
+            }
+        ).catch(
+            err => {
+                console.log(err)
+                alert("Couldn't Log Out")
+            })
+    }
+
+    render() {
+        if (!this.state.loaded) {
+            return (
+                <Loader active />
+            );
+        }
+
+
+    }
 }
 
 export default Logout
